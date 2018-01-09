52b817c4ba6a46d71794185b9d64db83d2ff84fc
XJIE6 <krav4enkoyura@yandex.ru>
2018-01-09 23:28:03 +0300

    anko support added

--- AndroidController/app/src/main/java/ru/spbau/mit/androidcontroller/MainActivity.kt
+++ AndroidController/app/src/main/java/ru/spbau/mit/androidcontroller/MainActivity.kt
@@ -2,11 +2,19 @@ package ru.spbau.mit.androidcontroller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import org.jetbrains.anko.*
+import org.jetbrains.anko.sdk25.coroutines.onClick
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val layout = verticalLayout {
+            val name = editText()
+            button("Say Hello") {
+                onClick { toast("Hello, ${name.text}!") }
+            }
+        }
+        setContentView(layout)
     }
 }
